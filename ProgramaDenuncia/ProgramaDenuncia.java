eb1c713dbc1ed0f071e42433b895ea9e4024f507
CristianZambra <cristianmartinez202@gmail.com>
2024-03-04 01:26:54 -0600

    agregar nuevo

--- ProgramaDenuncia/ProgramaDenuncia.java
+++ ProgramaDenuncia/ProgramaDenuncia.java
@@ -7,8 +7,15 @@ public class ProgramaDenuncia {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        // Datos para los crimenes previos
+        Crimen crimen1 = new Crimen("Asesinato", "Descripción del asesinato");
+        Crimen crimen2 = new Crimen("Robo", "Descripción del robo");
+        Crimen crimen3 = new Crimen("Violencia doméstica", "Descripción de la violencia doméstica");
+        String crimenesPrevios = crimen1.toString() + ", " + crimen2.toString() + ", " + crimen3.toString();
 
 
+       // Datos para oficial encargado
+       String ooficialencargado = "Oficial Mario";
 
         
     }
