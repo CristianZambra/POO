9268a7e2ed1d29aef4da501255d0e9d00efbda0c
CristianZambra <cristianmartinez202@gmail.com>
2024-03-04 00:18:54 -0600

    agregar ProgramaDenuncia

--- ProgramaDenuncia/Denuncia.java
+++ ProgramaDenuncia/Denuncia.java
@@ -2,13 +2,10 @@ package ProgramaDenuncia;
 /**
  * Denuncia
  */
-import java.util.Scanner;
+
 
 public class Denuncia {
-public static void main(String[] args) {
-    Scanner scanner = new Scanner(System.in);
-
     
-}
+
     
 }
---
+++ ProgramaDenuncia/ProgramaDenuncia.java
@@ -0,0 +1,5 @@
+package ProgramaDenuncia;
+
+public class ProgramaDenuncia {
+    
+}
