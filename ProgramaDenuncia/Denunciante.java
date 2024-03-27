7165c4606434e757a46a816ed8acfbfad52383af
CristianZambra <cristianmartinez202@gmail.com>
2024-03-26 18:34:22 -0600

    Denunciante

--- ProgramaDenuncia/Denunciante.java
+++ ProgramaDenuncia/Denunciante.java
@@ -5,5 +5,15 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class Denunciante {
-    
+    private String nombreCompleto;
+    private int numeroID;
+    private String direccionDomicilio;
+    private String tipoDenuncia;
+    private String sexo;
+
+
+
+
+
+
 }
