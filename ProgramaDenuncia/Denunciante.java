5c7d1c24faf6611e57f7a98f028cde522dbe64af
CristianZambra <cristianmartinez202@gmail.com>
2024-03-26 18:35:54 -0600

    Denunciante

--- ProgramaDenuncia/Denunciante.java
+++ ProgramaDenuncia/Denunciante.java
@@ -11,7 +11,7 @@ public class Denunciante {
     private String tipoDenuncia;
     private String sexo;
 
-    
+
     public Denunciante(String nombreCompleto, int numeroID, String direccionDomicilio, String tipoDenuncia,
             String sexo) {
         this.nombreCompleto = nombreCompleto;
@@ -22,8 +22,54 @@ public class Denunciante {
     }
 
 
+    public String getNombreCompleto() {
+        return nombreCompleto;
+    }
 
 
+    public void setNombreCompleto(String nombreCompleto) {
+        this.nombreCompleto = nombreCompleto;
+    }
+
+
+    public int getNumeroID() {
+        return numeroID;
+    }
+
+
+    public void setNumeroID(int numeroID) {
+        this.numeroID = numeroID;
+    }
+
+
+    public String getDireccionDomicilio() {
+        return direccionDomicilio;
+    }
+
+
+    public void setDireccionDomicilio(String direccionDomicilio) {
+        this.direccionDomicilio = direccionDomicilio;
+    }
+
+
+    public String getTipoDenuncia() {
+        return tipoDenuncia;
+    }
+
+
+    public void setTipoDenuncia(String tipoDenuncia) {
+        this.tipoDenuncia = tipoDenuncia;
+    }
+
+
+    public String getSexo() {
+        return sexo;
+    }
+
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
 
 
 }
