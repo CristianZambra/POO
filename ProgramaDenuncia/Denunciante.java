3707905cf5626e2d9ae8e046c58bd1963b82c1fa
CristianZambra <cristianmartinez202@gmail.com>
2024-03-26 18:35:12 -0600

    Denunciante

--- ProgramaDenuncia/Denunciante.java
+++ ProgramaDenuncia/Denunciante.java
@@ -11,6 +11,16 @@ public class Denunciante {
     private String tipoDenuncia;
     private String sexo;
 
+    
+    public Denunciante(String nombreCompleto, int numeroID, String direccionDomicilio, String tipoDenuncia,
+            String sexo) {
+        this.nombreCompleto = nombreCompleto;
+        this.numeroID = numeroID;
+        this.direccionDomicilio = direccionDomicilio;
+        this.tipoDenuncia = tipoDenuncia;
+        this.sexo = sexo;
+    }
+
 
 
 
