b6f741c6b20e0d656dd5a7db64d3285a8eadf401
CristianZambra <cristianmartinez202@gmail.com>
2024-03-26 18:07:41 -0600

    nuevo

---
+++ ProgramaDenuncia/Denunciado.java
@@ -0,0 +1,28 @@
+package ProgramaDenuncia;
+
+import java.util.Scanner;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+
+
+
+public class Denunciado {
+    private String nombreCompleto;
+    private int numeroID;
+    private String direccionDomicilio;
+    private String sexo;
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
