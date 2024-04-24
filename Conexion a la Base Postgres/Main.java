d1a7b93f93b5ea57339d540a55806f11a76d3dd3
Cristian Zambrano <60454759+CristianZambra@users.noreply.github.com>
2024-04-24 00:18:12 -0600

    Add files via upload

---
+++ Conexion a la Base Postgres/Main.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package conexionpostgressql;
+
+/**
+ *
+ * @author cerra
+ */
+public class Main {
+    public static void Main(String[] args){
+        conexion Conexion = new conexion();
+        Conexion.getconexion();
+    }
+}
